0b6660b6501a592163e0eadb5647873dc1bf2f41
patrick <patrickesteban.valenzuelahernan@gmail.com>
2016-02-22 11:48:09 -0400

    valida 1

--- Velocimetro/ViewController.swift
+++ Velocimetro/ViewController.swift
@@ -8,64 +8,19 @@
 
 import UIKit
 
-enum Velocidades: Int{
-    case Apagado = 0, VelocidadBaja = 20, VelocidadMedia = 50, VelocidadAlta = 120
-    
-    init (velocidadInicial: Velocidades){
-        self = velocidadInicial
-    }
-    
-}
+class ViewController: UIViewController {
+    //modif 1
 
-class Auto {
-    
-    var velocidad: Velocidades
-    
-    init ()
-    {
-        self.velocidad = Velocidades.Apagado
-        
-    }
-    func cambioDeVelocidad() ->(actual: Int, velocidadEnCadena: String){
-        var estado : String = ""
-        
-        if(velocidad.rawValue == 0){
-            estado = "\(velocidad.rawValue) + Apagado"
-            self.velocidad = Velocidades.VelocidadBaja
-        }
-        if(velocidad.rawValue == 20){
-            
-            self.velocidad = Velocidades.VelocidadMedia
-            estado =  "\(velocidad.rawValue) + Velocidad Media"
-            
-        };  if(velocidad.rawValue == 50){
-            self.velocidad = Velocidades.VelocidadAlta
-            estado =   "\(velocidad.rawValue) + Velocidad Baja"
-            
-        }; if(velocidad.rawValue == 120){
-            estado = "\(velocidad.rawValue) + Velocidad Alta"
-            self.velocidad = Velocidades.VelocidadMedia
-        }
-        return(self.velocidad.rawValue, estado)
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
     }
 
-var auto = Auto()
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
 
-for n in 1...20{
-    auto.velocidad = Velocidades.VelocidadBaja
-    print (n, auto.cambioDeVelocidad())
+
 }
-
-}
-
-
-
-
-
-
-
-
-
-
-
 
