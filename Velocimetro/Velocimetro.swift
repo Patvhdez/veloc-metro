a9ec4d344f1f71d2d0a02ba6a2ed2b4fe5a05ba9
patrick <patrickesteban.valenzuelahernan@gmail.com>
2016-02-22 11:56:32 -0400

    Este es mi minirreto

---
+++ Velocimetro/Velocimetro.swift
@@ -0,0 +1,69 @@
+//
+//  ViewController.swift
+//  Velocimetro
+//
+//  Created by Patrick on 22/2/16.
+//  Copyright © 2016 Patrick. All rights reserved.
+//
+
+import UIKit
+
+
+enum Velocidades: Int{
+    case Apagado = 0, VelocidadBaja = 20, VelocidadMedia = 50, VelocidadAlta = 120
+    
+    init (velocidadInicial: Velocidades){
+        self = velocidadInicial
+    }
+    
+}
+
+class Auto {
+    
+    var velocidad: Velocidades
+    
+    init ()
+    {
+        self.velocidad = Velocidades.Apagado
+        
+    }
+    func cambioDeVelocidad() ->(actual: Int, velocidadEnCadena: String){
+        var estado : String = ""
+        
+        if(velocidad.rawValue == 0){
+            estado = "\(velocidad.rawValue) + Apagado"
+            self.velocidad = Velocidades.VelocidadBaja
+        }
+        if(velocidad.rawValue == 20){
+            
+            self.velocidad = Velocidades.VelocidadMedia
+            estado =  "\(velocidad.rawValue) + Velocidad Media"
+            
+        };  if(velocidad.rawValue == 50){
+            self.velocidad = Velocidades.VelocidadAlta
+            estado =   "\(velocidad.rawValue) + Velocidad Baja"
+            
+        }; if(velocidad.rawValue == 120){
+            estado = "\(velocidad.rawValue) + Velocidad Alta"
+            self.velocidad = Velocidades.VelocidadMedia
+        }
+        return(self.velocidad.rawValue, estado)
+    }
+}
+var auto = Auto()
+
+for n in 1...20{
+    auto.velocidad = Velocidades.VelocidadBaja
+    print (n, auto.cambioDeVelocidad())
+}
+
+
+
+
+
+
+
+
+
+
+
